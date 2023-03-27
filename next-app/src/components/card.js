ab140f237f55653ef7f8a67d19b9d01666d1871d
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-03-27 11:12:31 -0700

    Got grid rendering working

--- next-app/src/components/card.js
+++ next-app/src/components/card.js
@@ -1,28 +1,18 @@
 import React from "react";
 
-const style = {
-  card: {
-    background: "#F5F5F5",
-    borderRadius: "8px",
-    overflow: "hidden",
-    display: "flex",
-    flexDirection: "column",
-    // boxShadow: "0 0 10px 5px white"
-  },
-  imageContainer: {
-    height: "340px",
-    overflow: "hidden",
-  },
+const styles = {
+  card: "bg-white rounded-lg overflow-hidden flex flex-col m-1",
+  imageContainer: "h-96 overflow-hidden",
 };
 
-const card = () => {
+const Card = () => {
   return (
-    <div className={style.card}>
-        <div className={style.imageContainer}>
-
-        </div>
+    <div className={styles.card}>
+      <div className={styles.imageContainer}>
+        Test
+      </div>
     </div>
-  )
+  );
 };
 
-export default card;
+export default Card;
