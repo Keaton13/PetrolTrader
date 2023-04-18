5df03bfbd55e6c846a4437115adc071b5759454c
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-04-17 22:37:18 -0700

    Redeployed contracts and working on events for frontend

--- truffle-app/migrations/01_deploy.js
+++ truffle-app/migrations/01_deploy.js
@@ -4,7 +4,7 @@ const Mint = artifacts.require("MintCar");
 module.exports = function(deployer) {
   deployer.deploy(Mint).then(() => {
     console.log(Mint.address)
-    return deployer.deploy(Dealership,Mint.address,"0x84125D450F39d06C3Df31161D6d7de5c5977F472");
+    return deployer.deploy(Dealership,Mint.address,"0xbc57BAEd94eFac14c1F4172748313ef3DCf75c30");
   }).then(() => {
     console.log("Contracts deployed successfully!");
   });
