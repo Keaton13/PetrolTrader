85738f5647238cae0619135f7aad2109ce78969a
Keaton Krieger <keatonkrieger@Keatons-MacBook-Pro.local>
2023-05-15 17:19:40 -0700

    Changed gas to gas price, fixed next.config.js

--- truffle-app/migrations/01_deploy.js
+++ truffle-app/migrations/01_deploy.js
@@ -3,7 +3,7 @@ const Mint = artifacts.require("MintCar");
 
 module.exports = function(deployer) {
   deployer.deploy(Mint, {from: '0x14d9996764Bb3CcFfB989Af6C7691690bF8037Ac', gas: 5000000}).then(() => {
-    console.log(Mint.address)
+    // console.log(Mint.address)
     return deployer.deploy(Dealership, Mint.address, {from: '0x14d9996764Bb3CcFfB989Af6C7691690bF8037Ac', gas: 5000000});
   }).then(() => {
     console.log("Contracts deployed successfully!");
